9adf9c24a1e8fb29cca9aeb101ccf2975b72dcec
LehaoLin <lehaolin@link.cuhk.edu.cn>
2023-09-15 19:57:07 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -724,136 +724,136 @@ export const useStore = defineStore("store", {
     },
 
     // game auction
-    async identity() {
-      let web3 = new Web3(window.ethereum);
-      let contract_paint = new web3.eth.Contract(
-        abi_painting,
-        contractAddress_painting
-      );
-      let player_addr = await web3.currentProvider.selectedAddress;
-      let ntf_holder = await contract_paint.methods.checkowner(1).call();
+    // async identity() {
+    //   let web3 = new Web3(window.ethereum);
+    //   let contract_paint = new web3.eth.Contract(
+    //     abi_painting,
+    //     contractAddress_painting
+    //   );
+    //   let player_addr = await web3.currentProvider.selectedAddress;
+    //   let ntf_holder = await contract_paint.methods.checkowner(1).call();
 
-      let contract_market = new web3.eth.Contract(
-        abi_market,
-        contractAddress_market
-      );
-      let ex_holder = await contract_market.methods
-        .checkpreseller()
-        .call({ from: player_addr });
+    //   let contract_market = new web3.eth.Contract(
+    //     abi_market,
+    //     contractAddress_market
+    //   );
+    //   let ex_holder = await contract_market.methods
+    //     .checkpreseller()
+    //     .call({ from: player_addr });
 
-      if (
-        player_addr.toLowerCase() != ntf_holder.toLowerCase() &&
-        ex_holder == 0
-      ) {
-        return "buyer";
-      }
-      if (player_addr.toLowerCase() == ntf_holder.toLowerCase()) {
-        return "holder";
-      }
-      if (ex_holder > 0) {
-        return "preseller";
-      }
-    },
-    async shangjia() {
-      let web3 = new Web3(window.ethereum);
-      let contract = new web3.eth.Contract(abi_market, contractAddress_market);
-      // let player_addr = await web3.currentProvider.selectedAddress;
-      let nft_state = await contract.methods.checkstate().call();
-      if (nft_state) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    async nft_price() {
-      let web3 = new Web3(window.ethereum);
-      let contract = new web3.eth.Contract(abi_market, contractAddress_market);
-      let price = await contract.methods.checkprice().call();
-      price = web3.utils.fromWei(price, "ether");
-      return price;
-    },
-    async nft_auth() {
-      let web3 = new Web3(window.ethereum);
-      let contract = new web3.eth.Contract(abi_market, contractAddress_market);
-      let auth = await contract.methods.check_approve_market().call();
-      return auth;
-    },
-    async benefit() {
-      let web3 = new Web3(window.ethereum);
-      var player_addr = web3.currentProvider.selectedAddress; //目前网页链接的钱包地址，返回的是string
-      let contract = new web3.eth.Contract(abi_market, contractAddress_market);
-      await contract.methods.benefit().send({ from: player_addr });
-    },
-    async cancelList() {
-      let web3 = new Web3(window.ethereum);
-      var player_addr = web3.currentProvider.selectedAddress; //目前网页链接的钱包地址，返回的是string
-      let contract = new web3.eth.Contract(abi_market, contractAddress_market);
-      await contract.methods.cancelListing().send({ from: player_addr });
-    },
-    async upload_price() {
-      let web3 = new Web3(window.ethereum);
-      var player_addr = web3.currentProvider.selectedAddress; //目前网页链接的钱包地址，返回的是string
-      let contract = new web3.eth.Contract(abi_market, contractAddress_market);
-      await contract.methods
-        .upload_price(parseFloat(this.uploadPrice) * 10 ** 18)
-        .send({ from: player_addr });
-    },
-    async check_svg() {
-      let web3 = new Web3(window.ethereum);
-      // let player_addr = web3.currentProvider.selectedAddress;
-      let contract = new web3.eth.Contract(
-        abi_painting,
-        contractAddress_painting
-      );
-      let svg = await contract.methods.checksvg().call();
-      return svg;
-    },
-    async buy(price) {
-      let web3 = new Web3(window.ethereum);
-      var player_addr = web3.currentProvider.selectedAddress; //目前网页链接的钱包地址，返回的是string
-      let contract = new web3.eth.Contract(abi_market, contractAddress_market);
-      await contract.methods.buyItem().send({
-        from: player_addr,
-        value: web3.utils.toWei(price, "ether"),
-      });
-    },
-    async check_recent_seller() {
-      let web3 = new Web3(window.ethereum);
-      var player_addr = web3.currentProvider.selectedAddress; //目前网页链接的钱包地址，返回的是string
-      let contract = new web3.eth.Contract(abi_market, contractAddress_market);
-      let address = await contract.methods.checkrecentseller().call();
-      return address;
-    },
+    //   if (
+    //     player_addr.toLowerCase() != ntf_holder.toLowerCase() &&
+    //     ex_holder == 0
+    //   ) {
+    //     return "buyer";
+    //   }
+    //   if (player_addr.toLowerCase() == ntf_holder.toLowerCase()) {
+    //     return "holder";
+    //   }
+    //   if (ex_holder > 0) {
+    //     return "preseller";
+    //   }
+    // },
+    // async shangjia() {
+    //   let web3 = new Web3(window.ethereum);
+    //   let contract = new web3.eth.Contract(abi_market, contractAddress_market);
+    //   // let player_addr = await web3.currentProvider.selectedAddress;
+    //   let nft_state = await contract.methods.checkstate().call();
+    //   if (nft_state) {
+    //     return true;
+    //   } else {
+    //     return false;
+    //   }
+    // },
+    // async nft_price() {
+    //   let web3 = new Web3(window.ethereum);
+    //   let contract = new web3.eth.Contract(abi_market, contractAddress_market);
+    //   let price = await contract.methods.checkprice().call();
+    //   price = web3.utils.fromWei(price, "ether");
+    //   return price;
+    // },
+    // async nft_auth() {
+    //   let web3 = new Web3(window.ethereum);
+    //   let contract = new web3.eth.Contract(abi_market, contractAddress_market);
+    //   let auth = await contract.methods.check_approve_market().call();
+    //   return auth;
+    // },
+    // async benefit() {
+    //   let web3 = new Web3(window.ethereum);
+    //   var player_addr = web3.currentProvider.selectedAddress; //目前网页链接的钱包地址，返回的是string
+    //   let contract = new web3.eth.Contract(abi_market, contractAddress_market);
+    //   await contract.methods.benefit().send({ from: player_addr });
+    // },
+    // async cancelList() {
+    //   let web3 = new Web3(window.ethereum);
+    //   var player_addr = web3.currentProvider.selectedAddress; //目前网页链接的钱包地址，返回的是string
+    //   let contract = new web3.eth.Contract(abi_market, contractAddress_market);
+    //   await contract.methods.cancelListing().send({ from: player_addr });
+    // },
+    // async upload_price() {
+    //   let web3 = new Web3(window.ethereum);
+    //   var player_addr = web3.currentProvider.selectedAddress; //目前网页链接的钱包地址，返回的是string
+    //   let contract = new web3.eth.Contract(abi_market, contractAddress_market);
+    //   await contract.methods
+    //     .upload_price(parseFloat(this.uploadPrice) * 10 ** 18)
+    //     .send({ from: player_addr });
+    // },
+    //   async check_svg() {
+    //     let web3 = new Web3(window.ethereum);
+    //     // let player_addr = web3.currentProvider.selectedAddress;
+    //     let contract = new web3.eth.Contract(
+    //       abi_painting,
+    //       contractAddress_painting
+    //     );
+    //     let svg = await contract.methods.checksvg().call();
+    //     return svg;
+    //   },
+    //   async buy(price) {
+    //     let web3 = new Web3(window.ethereum);
+    //     var player_addr = web3.currentProvider.selectedAddress; //目前网页链接的钱包地址，返回的是string
+    //     let contract = new web3.eth.Contract(abi_market, contractAddress_market);
+    //     await contract.methods.buyItem().send({
+    //       from: player_addr,
+    //       value: web3.utils.toWei(price, "ether"),
+    //     });
+    //   },
+    //   async check_recent_seller() {
+    //     let web3 = new Web3(window.ethereum);
+    //     var player_addr = web3.currentProvider.selectedAddress; //目前网页链接的钱包地址，返回的是string
+    //     let contract = new web3.eth.Contract(abi_market, contractAddress_market);
+    //     let address = await contract.methods.checkrecentseller().call();
+    //     return address;
+    //   },
 
-    async auction_open() {
-      let web3 = new Web3(window.ethereum);
-      let contract = new web3.eth.Contract(
-        abi_painting,
-        contractAddress_painting
-      );
-      let result = await contract.methods.checkmarketopen().call();
-      return result;
-    },
+    //   async auction_open() {
+    //     let web3 = new Web3(window.ethereum);
+    //     let contract = new web3.eth.Contract(
+    //       abi_painting,
+    //       contractAddress_painting
+    //     );
+    //     let result = await contract.methods.checkmarketopen().call();
+    //     return result;
+    //   },
 
-    async get_sell_history() {
-      let web3 = new Web3(window.ethereum);
-      let contract = new web3.eth.Contract(abi_market, contractAddress_market);
-      let num = await contract.methods.checknum_sell().call();
-      let all_history = [];
-      for (let i = 1; i <= num; i++) {
-        let history = await contract.methods.check_selllist(num).call();
-        all_history.push(history);
-      }
-      return all_history;
-    },
-    async approve_market() {
-      let web3 = new Web3(window.ethereum);
-      let contract = new web3.eth.Contract(
-        abi_painting,
-        contractAddress_painting
-      );
-      let player_addr = web3.currentProvider.selectedAddress; //目前网页链接的钱包地址，返回的是string
-      await contract.methods.approve_market().send({ from: player_addr });
-    },
+    //   async get_sell_history() {
+    //     let web3 = new Web3(window.ethereum);
+    //     let contract = new web3.eth.Contract(abi_market, contractAddress_market);
+    //     let num = await contract.methods.checknum_sell().call();
+    //     let all_history = [];
+    //     for (let i = 1; i <= num; i++) {
+    //       let history = await contract.methods.check_selllist(num).call();
+    //       all_history.push(history);
+    //     }
+    //     return all_history;
+    //   },
+    //   async approve_market() {
+    //     let web3 = new Web3(window.ethereum);
+    //     let contract = new web3.eth.Contract(
+    //       abi_painting,
+    //       contractAddress_painting
+    //     );
+    //     let player_addr = web3.currentProvider.selectedAddress; //目前网页链接的钱包地址，返回的是string
+    //     await contract.methods.approve_market().send({ from: player_addr });
+    //   },
   },
 });
